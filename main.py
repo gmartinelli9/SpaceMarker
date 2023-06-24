3456afcf1059a55d420f6714614d7dd982bc1289
Alisson <1134723@atitus.org.br>
2023-06-24 01:32:36 -0300

    Fundo, Imagens e alguns ajustes

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import pygame
 import winsound
-pygame.init()
-
-tamanho = (1000,563)
-tela =  pygame.display.set_mode( tamanho )
-pygame.display.set_caption("SPACE MARKER")
-clock = pygame.time.Clock()
-space = pygame.image.load("space.png")
-pygame.display.set_icon(space)
 from tkinter import simpledialog
+import time
 
-def jogo():
+pygame.init()
+tamanho = (1000,563)
+branco = (255,255,255)
+tela =  pygame.display.set_mode( tamanho )
+clock = pygame.time.Clock()
+pygame.display.set_caption("SPACE MARKER")
+space = pygame.image.load("space.png")
+fundo = pygame.image.load("bg.jpg")
+pygame.display.set_icon(space)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            item = simpledialog.askstring("Space", "Nome da Estrela:")
+            print(item)
+            if item == None:
+                item = "Desconhecido"+str(pos)
+            estrelas[item] = pos
     
+
+
+    #TELA
+    tela.fill(branco)
+    tela.blit(fundo,(0,0))
+    pygame.display.update()
+    clock.tick(60)
