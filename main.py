6c302f9a75cdc68025974e56963f4961e6058679
Alisson <1134723@atitus.org.br>
2023-06-24 16:31:12 -0300

    Desenhando as Estrelas

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 pygame.init()
 tamanho = (1000,563)
 branco = (255,255,255)
+estrelas= {}
+raiodocirculo = 10
 tela =  pygame.display.set_mode( tamanho )
 clock = pygame.time.Clock()
 pygame.display.set_caption("SPACE MARKER")
@@ -19,17 +21,26 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-        elif event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            posicaomouse = pygame.mouse.get_pos()
             item = simpledialog.askstring("Space", "Nome da Estrela:")
             print(item)
+            estrelas[item] = posicaomouse
             if item == None:
-                item = "Desconhecido"+str(pos)
-            estrelas[item] = pos
-    
+                item = "Desconhecido"+str(posicaomouse)
+            estrelas[item] = posicaomouse
+
+    for item,posicao in estrelas.items():
+        pygame.draw.circle(tela,branco,posicao,raiodocirculo)
+        pygame.draw.line(tela,branco,list(estrelas.values())[0], posicao)
+        fonte = pygame.font.Font(None,20)
+        texto = fonte.render(item,True,(255,255,255)) 
+        tela.blit(texto,(posicao[0]+10, posicao[1]+ 10))
 
 
     #TELA
+    pygame.display.flip()
     tela.fill(branco)
     tela.blit(fundo,(0,0))
     pygame.display.update()
