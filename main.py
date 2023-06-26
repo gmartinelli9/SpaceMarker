dabc3adda7da2fedb0d375d1e803acb89b419fd7
Alisson <1134723@atitus.org.br>
2023-06-25 22:14:07 -0300

    Funções e som

--- main.py
+++ main.py
@@ -1,26 +1,69 @@
 import pygame
 import winsound
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
 import time
 
 pygame.init()
 tamanho = (1000,563)
 branco = (255,255,255)
+fonte = pygame.font.SysFont("Georgia", 1)
 estrelas= {}
+marcacoes = []
 raiodocirculo = 10
 tela =  pygame.display.set_mode( tamanho )
 clock = pygame.time.Clock()
 pygame.display.set_caption("SPACE MARKER")
 space = pygame.image.load("space.png")
 fundo = pygame.image.load("bg.jpg")
+som = pygame.mixer.Sound("Space_Machine_Power.mp3")
+som.play(-1)
 pygame.display.set_icon(space)
 
+def salvarmark():
+    with open('Estrelas Marcadas.txt', 'w') as arquivo:
+        for posicao, nome in marcacoes:
+            arquivo.write(f"{posicao[0]}, {posicao[1]}, {nome}\n ")
+
+def carregarmark():
+    marcacoes.clear()
+    try:
+        with open('Estrelas Marcadas.txt', 'r') as arquivo:
+            for linha in arquivo:
+                x,y,nome = linha.strip().split(",")
+                posicao = (int(x), int(y))
+                marcacoes.append((posicao,nome))
+    except FileNotFoundError:
+        messagebox.showinfo("Erro", "As últimas marcações não foram encontradas")
+
+def excluirmark():
+    marcacoes.clear()
+
+def escritanatela():
+    opcoes = fonte.render("Opções:", True, branco)
+    salvar = fonte.render("F10 - Para salvar as marcações", True, branco)
+    carregar = fonte.render("F11 - Para carregar as últimas marcações", True, branco)
+    excluir = fonte.render("F12 - Para excluir todas as marcações", True, branco)
+    tela.blit (opcoes,(10,10))
+    tela.blit (salvar,(10,40))
+    tela.blit (carregar,(10,70))
+    tela.blit (excluir,(10,100))
 
 while True:
+    escritanatela()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+            elif event.key == pygame.K_F10:
+                salvarmark
+            elif event.key == pygame.K_F11:
+                carregarmark
+            elif event.key == pygame.K_F12:
+                excluirmark
+
 
         if event.type == pygame.MOUSEBUTTONUP:
             posicaomouse = pygame.mouse.get_pos()
@@ -41,7 +84,5 @@ while True:
 
     #TELA
     pygame.display.flip()
-    tela.fill(branco)
     tela.blit(fundo,(0,0))
     pygame.display.update()
-    clock.tick(60)
