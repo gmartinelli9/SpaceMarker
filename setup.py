c4bfa674c53c0ef1a27d2c16f150fe886a2cd13e
Alisson <1134723@atitus.org.br>
2023-06-26 23:31:16 -0300

    Versão quase Final

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import cx_Freeze
-executables = [
-    cx_Freeze.Executable(script="main.py", icon="space.ico")
-]
+
+executables = [cx_Freeze.Executable('main.py')]
+
 cx_Freeze.setup(
-    name = "Space Maker",
-    options = {
-        "build_exe":{
-            "packages": ["pygame"]
-        }
-    } , executables = executables
+    name="Space Maker",
+    options={'build_exe': {'packages':['pygame'],
+                           'include_files':['bg.jpg', 'Space_Machine_Power.mp3', 'space.ico', 'space.png']}},
+
+    executables = executables
 )
+    
